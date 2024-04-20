b234fdcc6b6abc730ab3124c050d831475d2842c
Emman-pip <115330041+Emman-pip@users.noreply.github.com>
2024-04-20 11:16:39 +0800

    Add telescope.

--- init.lua
+++ init.lua
@@ -23,5 +23,13 @@ vim.keymap.set("n", "<Leader>b", ":bprevious<cr>")
 vim.keymap.set("n", "<Leader>n", ":bnext<cr>")
 vim.keymap.set("n", "<Leader>d", ":bdelete<cr>")
 
+
 require("plugins")
+
+-- telescope keymaps
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -74,7 +74,11 @@ require("lazy").setup({
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
 		"neovim/nvim-lspconfig",
-	}
+	},
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
 
 })
 
